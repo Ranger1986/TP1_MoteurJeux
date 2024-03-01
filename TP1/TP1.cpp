425250a38f79d5842009e79968f85429362c28de
Ranger1986 <pierre6richard7@gmail.com>
2024-03-01 17:31:27 +0100

    styke: all in TP1

--- TP1/TP1.cpp
+++ TP1/TP1.cpp
@@ -18,12 +18,11 @@ GLFWwindow* window;
 #include <iostream>
 
 using namespace glm;
+using namespace std;
 
 #include <common/objloader.hpp>
 #include <common/shader.hpp>
 #include <common/vboindexer.hpp>
-
-#include "src/Plan.h"
 
 void processInput(GLFWwindow* window);
 // settings
@@ -47,6 +46,59 @@ bool camLibre = true;
 
 bool mouse_pressed = false;
 /*******************************************************************************/
+class Plan
+{
+private:
+    vec3 center;
+    int width_vertices;
+    int length_vertices;
+    float vertices_distance;
+    vector<vector<float>> heights;
+public:
+    Plan(vec3 center, int width_vertices, int length_vertices);
+    void setRandomHeight();
+    void drawPlan(std::vector<glm::vec3>& indexed_vertices, std::vector<unsigned short>& indices);
+};
+Plan::Plan(vec3 center, int width_vertices, int length_vertices) {
+    this->center = center;
+    this->width_vertices = width_vertices;
+    this->length_vertices = length_vertices;
+    this->vertices_distance = 1;
+    for (int i = 0; i < width_vertices; i++) {
+        vector<float> witdh_height;
+        for (int i = 0; i < length_vertices; i++) {
+            witdh_height.push_back(0);
+        }
+        heights.push_back(witdh_height);
+    }
+}
+void Plan::setRandomHeight() {
+    for (int i = 0; i < width_vertices; i++) {
+        for (int j = 0; j < length_vertices; j++) {
+            heights[i][j]=static_cast<float>(rand())/RAND_MAX-0.5;
+        }
+    }
+}
+void Plan::drawPlan(std::vector<glm::vec3>& indexed_vertices, std::vector<unsigned short>& indices) {
+    for (int i = 0; i < width_vertices; i++) {
+        for (int j = 0; j < length_vertices; j++) {
+            indexed_vertices.push_back(center + 
+                vec3(vertices_distance / (length_vertices - 1) * j,
+                    heights[i][j],
+                    vertices_distance / (width_vertices - 1) * i));
+        }
+    }
+    for (int i = 0; i < width_vertices - 1; i++) {
+        for (int j = 0; j < length_vertices - 1; j++) {
+            indices.push_back(i * length_vertices + j);
+            indices.push_back((i + 1) * length_vertices + j);
+            indices.push_back((i + 1) * length_vertices + j + 1);
+            indices.push_back(i * length_vertices + j);
+            indices.push_back(i * length_vertices + j + 1);
+            indices.push_back((i + 1) * length_vertices + j + 1);
+        }
+    }
+}
 
 int main(void) {
     // Initialise GLFW
