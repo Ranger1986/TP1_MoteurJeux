444e4f6b948d673047f27524ca668e86715fcba2
Ranger1986 <pierre6richard7@gmail.com>
2024-03-01 19:08:14 +0100

    feat: camera orbitale

--- TP1/TP1.cpp
+++ TP1/TP1.cpp
@@ -31,9 +31,12 @@ const unsigned int SCR_HEIGHT = 600;
 
 // camera
 glm::vec3 camera_position = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 camera_target = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 camera_target = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::vec3 camera_up = glm::vec3(0.0f, 1.0f, 0.0f);
-
+glm::vec3 camera_front = glm::vec3(0.0f, 0.0f, -1.0f);
+bool camlibre = true;
+bool camfixe = false;
+bool camorbitale = false;
 // timing
 float deltaTime = 0.0f;  // time between current frame and last frame
 float lastFrame = 0.0f;
@@ -42,19 +45,27 @@ float lastFrame = 0.0f;
 float angle = 0.;
 float zoom = 1.;
 
-bool camLibre = true;
-
-bool mouse_pressed = false;
 /*******************************************************************************/
-class Plan
-{
-private:
+// declaration des matrices composante de MVP
+mat4 modelMatrix;
+mat4 viewMatrix;
+mat4 projectionMatrix;
+// donnees 3d
+vector<unsigned short> indices;
+vector<vec3> indexed_vertices;
+// buffers
+GLuint vertexbuffer;
+GLuint elementbuffer;
+
+class Plan {
+   private:
     vec3 center;
     int width_vertices;
     int length_vertices;
     float vertices_distance;
     vector<vector<float>> heights;
-public:
+
+   public:
     Plan(vec3 center, int width_vertices, int length_vertices);
     void setRandomHeight();
     void drawPlan(std::vector<glm::vec3>& indexed_vertices, std::vector<unsigned short>& indices);
@@ -63,7 +74,7 @@ Plan::Plan(vec3 center, int width_vertices, int length_vertices) {
     this->center = center;
     this->width_vertices = width_vertices;
     this->length_vertices = length_vertices;
-    this->vertices_distance = 1;
+    this->vertices_distance = 2;
     for (int i = 0; i < width_vertices; i++) {
         vector<float> witdh_height;
         for (int i = 0; i < length_vertices; i++) {
@@ -75,17 +86,19 @@ Plan::Plan(vec3 center, int width_vertices, int length_vertices) {
 void Plan::setRandomHeight() {
     for (int i = 0; i < width_vertices; i++) {
         for (int j = 0; j < length_vertices; j++) {
-            heights[i][j]=static_cast<float>(rand())/RAND_MAX-0.5;
+            heights[i][j] = static_cast<float>(rand()) / RAND_MAX - 0.5;
         }
     }
 }
 void Plan::drawPlan(std::vector<glm::vec3>& indexed_vertices, std::vector<unsigned short>& indices) {
+    float half_width = (width_vertices - 1) * vertices_distance / 2.0f;
+    float half_length = (length_vertices - 1) * vertices_distance / 2.0f;
     for (int i = 0; i < width_vertices; i++) {
         for (int j = 0; j < length_vertices; j++) {
-            indexed_vertices.push_back(center + 
-                vec3(vertices_distance / (length_vertices - 1) * j,
-                    heights[i][j],
-                    vertices_distance / (width_vertices - 1) * i));
+            indexed_vertices.push_back(center +
+                                       vec3(vertices_distance * j - half_width,
+                                            heights[i][j],
+                                            vertices_distance * i - half_length));
         }
     }
     for (int i = 0; i < width_vertices - 1; i++) {
@@ -98,8 +111,16 @@ void Plan::drawPlan(std::vector<glm::vec3>& indexed_vertices, std::vector<unsign
             indices.push_back((i + 1) * length_vertices + j + 1);
         }
     }
-}
+    // Load it into a VBO
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, indexed_vertices.size() * sizeof(glm::vec3), &indexed_vertices[0], GL_STATIC_DRAW);
 
+    // Generate a buffer for the indices as well
+    glGenBuffers(1, &elementbuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned short), &indices[0], GL_STATIC_DRAW);
+}
 int main(void) {
     // Initialise GLFW
     if (!glfwInit()) {
@@ -162,38 +183,17 @@ int main(void) {
 
     /*****************TODO***********************/
     // Get a handle for our "Model View Projection" matrices uniforms
-
+    modelMatrix = mat4(1.f);
+    viewMatrix = mat4(1.f);
+    projectionMatrix = mat4(1.f);
     /****************************************/
-    std::vector<unsigned short> indices;  // Triangles concaténés dans une liste
-    std::vector<std::vector<unsigned short> > triangles;
-    std::vector<glm::vec3> indexed_vertices;
-
-    // Chargement du fichier de maillage
-    // std::string filename("chair.off");
-    // loadOFF(filename, indexed_vertices, indices, triangles );
 
     // Plan
     float vertices_cote = 16;
-    float longueur_cote = 1;
     vec3 center = vec3(0.0, 0.0, 0.0);
-    Plan plan = Plan(center,vertices_cote,vertices_cote);
+    Plan plan = Plan(center, vertices_cote, vertices_cote);
     plan.setRandomHeight();
     plan.drawPlan(indexed_vertices, indices);
-    std::cout << std::endl
-              << indices[256] << std::endl
-              << std::endl;
-
-    // Load it into a VBO
-    GLuint vertexbuffer;
-    glGenBuffers(1, &vertexbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, indexed_vertices.size() * sizeof(glm::vec3), &indexed_vertices[0], GL_STATIC_DRAW);
-
-    // Generate a buffer for the indices as well
-    GLuint elementbuffer;
-    glGenBuffers(1, &elementbuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned short), &indices[0], GL_STATIC_DRAW);
 
     // Get a handle for our "LightPosition" uniform
     glUseProgram(programID);
@@ -221,21 +221,17 @@ int main(void) {
         // Use our shader
         glUseProgram(programID);
 
-        /*****************TODO***********************/
-        // Model matrix : an identity matrix (model will be at the origin) then change
-        mat4 model(1.0f);
-        // View matrix : camera/view transformation lookat() utiliser camera_position camera_target camera_up
-        mat4 view = glm::lookAt(
-            camera_position,
-            camera_position+camera_target,
-            camera_up);
-        // Projection matrix : 45 Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-        mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-        // Send our transformation to the currently bound shader,
-        // in the "Model View Projection" to the shader uniforms
-        mat4 MVP = projection*view*model;
+        viewMatrix = lookAt(camera_position, camera_target, camera_up);        
+        projectionMatrix = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+        if (camorbitale)
+        {
+            viewMatrix=rotate(viewMatrix,radians(angle/10),camera_up);
+            angle++;
+        }
+        
+        mat4 MVP = projectionMatrix * viewMatrix * modelMatrix;
         GLuint MVPlocation = glGetUniformLocation(programID, "MVP");
-        glUniformMatrix4fv(MVPlocation,1,GL_FALSE,&MVP[0][0]);
+        glUniformMatrix4fv(MVPlocation, 1, GL_FALSE, &MVP[0][0]);
         /****************************************/
 
         // 1rst attribute buffer : vertices
@@ -289,69 +285,82 @@ void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS){
-        if(camLibre == true){
-            camLibre = false;
-        }else{
-            camLibre = true;
-        }
-    }
-
     // Vitesse de déplacement de la caméra
     float cameraSpeed = 2.5f * deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+        camlibre = true;
+        camfixe = false;
+        camorbitale = false;
+    }
 
-    // Déplacements caméra libre
-    if (camLibre == true){
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+        camlibre = false;
+        camfixe = true;
+        camorbitale = false;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
+        camlibre = false;
+        camfixe = false;
+        camorbitale = true;
+    }
+    if (camlibre) {
         // Déplacer la caméra vers l'avant
-        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS){
-            camera_position += cameraSpeed * camera_target;
+        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+            camera_position += cameraSpeed * normalize(camera_position-camera_target);
         }
 
         // Déplacer la caméra vers l'arrière
-        if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS){
-            camera_position -= cameraSpeed * camera_target;
+        if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+            camera_position -= cameraSpeed * normalize(camera_position-camera_target);
         }
 
         // Déplacer la caméra vers la droite
-        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-            camera_position += glm::normalize(glm::cross(camera_target, camera_up)) * cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+            camera_position += vec3{1, 0, 0} * cameraSpeed;
+            camera_target += vec3{1, 0, 0} * cameraSpeed;
         }
 
         // Déplacer la caméra vers la gauche
-        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-            camera_position -= glm::normalize(glm::cross(camera_target, camera_up)) * cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+            camera_position -= vec3{1, 0, 0} * cameraSpeed;
+            camera_target -= vec3{1, 0, 0} * cameraSpeed;
         }
 
         // Déplacer la caméra vers le haut
-        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-            camera_position += camera_up * cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+            camera_position += vec3{0, 1, 0} * cameraSpeed;
+            camera_target += vec3{0, 1, 0} * cameraSpeed;
         }
 
         // Déplacer la caméra vers le bas
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
-            camera_position -= camera_up * cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+            camera_position -= vec3{0, 1, 0} * cameraSpeed;
+            camera_target -= vec3{0, 1, 0} * cameraSpeed;
         }
-    }else{
-        // Déplacements caméra orbital (centré sur l'objet)
-        // Calcul de l'angle de rotation basé sur le temps écoulé pour une rotation constante
-        angle += 0.5f * deltaTime;
-
-        // Mettre à jour la position de la caméra pour la vue en rotation autour de l'origine
-        float radius = 3.0f; // Rayon de rotation
-        float camX = sin(angle) * radius;
-        float camZ = cos(angle) * radius;
-        camera_position = glm::vec3(camX, 0.0f, camZ);
-
-        // Définir la cible de la caméra pour regarder l'origine
-        camera_target = glm::normalize(glm::vec3(-camX, 0.0f, -camZ));
-
-        // Définir la matrice de vue pour afficher le terrain sous un angle de 45 degrés
-        mat4 view = glm::lookAt(
-            camera_position,       // Position de la caméra
-            camera_position - camera_target, // Point vers lequel la caméra est orientée (l'origine dans ce cas)
-            camera_up              // Vecteur "haut"
-        );
     }
+    if (camorbitale)
+    {
+        // Déplacer la caméra vers le haut
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+            camera_position += vec3{0, 1, 0} * cameraSpeed;
+        }
+
+        // Déplacer la caméra vers le bas
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+            camera_position -= vec3{0, 1, 0} * cameraSpeed;
+        }
+        // Déplacer la caméra vers l'avant
+        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+            camera_position += cameraSpeed * camera_front;
+        }
+
+        // Déplacer la caméra vers l'arrière
+        if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+            camera_position -= cameraSpeed * camera_front;
+        }
+    }
+    
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
